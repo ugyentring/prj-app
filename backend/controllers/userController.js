d24dd60ff0617a4f347cce8abbe907cb620577ab
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-27 00:15:39 +0600

    notification models

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,3 +16,37 @@ export const getUserProfile = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const followUnfollow = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res.status(400).json({ error: "You can't follow yourself" });
+    }
+
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: "User not found" });
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      //unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      //follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+
+      //send notifiaction to that user u follow
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    console.log("Error in the followUnfollow: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { getUserProfile } from "../controllers/userController.js";
+import {
+  getUserProfile,
+  followUnfollow,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 // router.get("/sugested", protectRoute, getUserProfile);
-// router.post("/follow/:id", protectRoute, followUnfollow);
+router.post("/follow/:id", protectRoute, followUnfollow);
 // router.post("/pudate", protectRoute, updateUserProfile);
 
 export default router;
