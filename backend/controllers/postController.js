44f20fa24ae836aa5e7db950064c3d33cf92560a
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-27 03:59:13 +0600

    delete post functionality done!

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -39,3 +39,39 @@ export const createPost = async (req, res) => {
     console.log("Error in createPost: ", error);
   }
 };
+
+//delete your post logic
+export const deletePost = async (req, res) => {
+  try {
+    //find post by ID
+    const post = await Post.findById(req.params.id);
+
+    //check if post exists
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    //you can only delete your post
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete the post" });
+    }
+
+    //delete image from cloudinary too
+    const imageId = post.img.split("/").pop().split(".")[0];
+    await cloudinary.uploader.destroy(imageId);
+
+    //delete from database
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("Error in delete post controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+//comment on a post
+export const commentPost = async (req, res) => {
+  
+  }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { createPost } from "../controllers/postController.js";
+import {
+  commentPost,
+  createPost,
+  deletePost,
+} from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 // router.post("/like/:id", protectRoute, likeUnlikePost);
-// router.post("/comment/:id", protectRoute, commentPost);
-// router.delete("/delete", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
