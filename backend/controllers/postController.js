34c7a2a1c864b30c3d95b6db9cb8ace4d56308b7
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-27 23:36:15 +0600

    donw with post routes and controllers

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -213,3 +213,26 @@ export const getFollowingPosts = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+//getting all the post of user
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({ path: "comments.user", select: "-password" });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in the getUserPosts controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -8,6 +8,7 @@ import {
   getLikedPosts,
   getFollowingPosts,
   likeUnlikePost,
+  getUserPosts,
 } from "../controllers/postController.js";
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const router = express.Router();
 router.get("/all", protectRoute, getAllPosts);
 router.get("/liked/:id", protectRoute, getLikedPosts);
 router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentPost);
