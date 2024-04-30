2119b3747dae71fd2949ce62cffa7011ad56671b
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-30 23:37:06 +0600

    done with follow unfollow functionality

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
+
+import useFollow from "../../hooks/useFollow";
 
 const RightPanel = () => {
   const { data: suggestedUsers, isLoading } = useQuery({
@@ -18,6 +22,8 @@ const RightPanel = () => {
       }
     },
   });
+
+  const { follow, isPending } = useFollow();
 
   if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
@@ -62,9 +68,12 @@ const RightPanel = () => {
                 <div>
                   <button
                     className="btn bg-green-700 text-white hover:bg-white hover:text-black hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
-                    Follow
+                    {isPending ? <LoadingSpinner /> : "Follow"}
                   </button>
                 </div>
               </Link>
