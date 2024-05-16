3ba46ce5325ca45ba8af77e1fc331505b48a8fcd
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-16 13:19:46 +0600

    donw with contract deployment

--- frontend/src/constants/constant.js
+++ frontend/src/constants/constant.js
@@ -1,4 +1,118 @@
-const contractAddress = "";
-const contractAbi = "";
+const contractAddress = "0xBDd24cBe58F9d340C49C650Fcc433FDDb8435255";
+const contractAbi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "reciever",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amt",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address payable",
+        name: "receiver",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+    ],
+    name: "addToBlockchain",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllTransactions",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "sender",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "reciever",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "message",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "timestamp",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Transaction.TransferStruct[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getTransactionCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 export { contractAddress, contractAbi };
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,14 +1,17 @@
+
 require("@nomiclabs/hardhat-waffle");
 
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const API_URL =
+  "https://eth-sepolia.g.alchemy.com/v2/r2QSBD2l6RBIJvw89Fit-sany_tVHQGP";
+const PRIVATE_KEY =
+  "3162ecdc20d711e35717bfe6d12abb5ba2ab7286e84cfac7c67bcf7ed95a1735";
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
