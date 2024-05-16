db2926aa023b3d712132d28d1d12203f50d3f0ce
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-16 20:34:39 +0600

    logo design

--- frontend/src/components/logo/Logo.jsx
+++ frontend/src/components/logo/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <span className="text-sm md:text-2xl lg:text-3xl xl:text-4xl font-extrabold font-sans">
+      <span className="text-sm md:text-2xl lg:text-3xl xl:text-4xl font-extrabold italic">
         NorbNode
       </span>
     </div>
