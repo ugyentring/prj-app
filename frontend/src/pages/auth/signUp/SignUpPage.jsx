4f7be74e2106e7fa502f9092e4ec3a4880228169
sapuna <12220064.gcit@rub.edu.bt>
2024-05-30 17:34:29 +0600

    changes in design Login signup

--- frontend/src/pages/auth/signUp/SignUpPage.jsx
+++ frontend/src/pages/auth/signUp/SignUpPage.jsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
-import { MdOutlineMail, MdPassword, MdVisibility, MdVisibilityOff } from "react-icons/md";
+import { MdOutlineMail, MdPassword, MdDriveFileRenameOutline } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 import toast from "react-hot-toast";
-
-//react query imports
 import { useMutation } from "@tanstack/react-query";
 
 const SignUpPage = () => {
@@ -13,11 +11,7 @@ const SignUpPage = () => {
     username: "",
     fullName: "",
     password: "",
-    confirmPassword: "",
   });
-
-  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false); // State to toggle confirm password visibility
 
   const { mutate, isError, isPending, error } = useMutation({
     mutationFn: async ({ email, username, fullName, password }) => {
@@ -31,8 +25,7 @@ const SignUpPage = () => {
         });
 
         const data = await res.json();
-        if (!res) throw new Error(data.error || "Failed to create account!");
-        // console.log(data);
+        if (!res.ok) throw new Error(data.error || "Failed to create account!");
         return data;
       } catch (error) {
         console.error(error);
@@ -46,32 +39,6 @@ const SignUpPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Check if all fields are filled
-    if (
-      !formData.email ||
-      !formData.username ||
-      !formData.fullName ||
-      !formData.password ||
-      !formData.confirmPassword
-    ) {
-      toast.error("Please fill in all fields");
-      return;
-    }
-
-    // Password validation
-    if (formData.password !== formData.confirmPassword) {
-      toast.error("Passwords do not match");
-      return;
-    }
-
-    if (!isPasswordValid(formData.password)) {
-      toast.error(
-        "Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 8 characters long."
-      );
-      return;
-    }
-
     mutate(formData);
   };
 
@@ -79,111 +46,89 @@ const SignUpPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevState) => !prevState);
-  };
-
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword((prevState) => !prevState);
-  };
-
-  // Password validation function
-  const isPasswordValid = (password) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-    return passwordRegex.test(password);
-  };
-
   return (
-    <div className="max-w-screen-xl mx-auto flex h-screen px-10">
-      <div className="flex-1 hidden lg:flex items-center  justify-center"></div>
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <form
-          className="lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="text-4xl font-extrabold text-black">SignUp.</h1>
-          <label className="input input-bordered rounded flex items-center gap-2">
-            <MdOutlineMail />
-            <input
-              type="email"
-              className="grow"
-              placeholder="Email"
-              name="email"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={formData.email}
-            />
-          </label>
-          <label className="input input-bordered rounded flex items-center gap-2">
-            <MdPassword />
-            <input
-              type="text"
-              className="grow"
-              placeholder="Username"
-              name="username"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={formData.username}
-            />
-          </label>
-          <label className="input input-bordered rounded flex items-center gap-2">
-            <MdPassword />
-            <input
-              type="text"
-              className="grow"
-              placeholder="Full Name"
-              name="fullName"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={formData.fullName}
-            />
-          </label>
-          <label className="input input-bordered rounded flex items-center gap-2">
-            <MdPassword />
-            <input
-              type={showPassword ? "text" : "password"} // Use conditional rendering based on showPassword state
-              className="grow"
-              placeholder="Password"
-              name="password"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={formData.password}
-            />
-            {showPassword ? (
-              <MdVisibilityOff onClick={togglePasswordVisibility} />
-            ) : (
-              <MdVisibility onClick={togglePasswordVisibility} />
-            )}
-          </label>
-          <label className="input input-bordered rounded flex items-center gap-2">
-            <MdPassword />
-            <input
-              type={showConfirmPassword ? "text" : "password"} // Use conditional rendering based on showConfirmPassword state
-              className="grow"
-              placeholder="Confirm Password"
-              name="confirmPassword"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={formData.confirmPassword}
-            />
-            {showConfirmPassword ? (
-              <MdVisibilityOff onClick={toggleConfirmPasswordVisibility} />
-            ) : (
-              <MdVisibility onClick={toggleConfirmPasswordVisibility} />
-            )}
-          </label>
-          <button className="btn rounded-full bg-green-700 text-white">
-            {isPending ? "Loading..." : "Sign Up"}
-          </button>
-          {isError && <p className="text-red-500">{error.message}</p>}
-        </form>
-        <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
-          <p className="text-black text-lg">
-            Already have an account?{" "}
-            <Link to="/login" className="text-blue-600">
-              Sign in
+    <div className="max-w-screen-xl mx-auto flex h-screen items-center justify-center">
+      <div className="flex">
+        <div className="hidden md:flex w-1/2 items-center justify-center bg-green-900 p-6">
+          <div className="text-white text-center">
+            <h2 className="text-3xl font-bold mb-4">Welcome to NorbNode</h2>
+            <p className="mb-6">
+              Already have an account? Sign in to access all the features.
+            </p>
+            <Link
+              to="/login"
+              className="bg-white text-green-900 font-bold py-2 px-4 rounded-lg hover:bg-green-900 hover:text-white transition-colors duration-300"
+            >
+              Sign In
             </Link>
-          </p>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 p-6">
+          <div className="bg-white rounded-3xl p-8 shadow-md">
+            <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
+              <h1 className="text-4xl font-extrabold text-black mb-6">Sign Up</h1>
+              <label className="input input-bordered rounded flex items-center gap-2">
+                <MdOutlineMail />
+                <input
+                  type="email"
+                  className="input-field flex-1"
+                  placeholder="Email"
+                  name="email"
+                  autoComplete="off"
+                  onChange={handleInputChange}
+                  value={formData.email}
+                />
+              </label>
+              <div className="flex gap-4 flex-wrap">
+                <label className="input input-bordered rounded flex items-center gap-2 flex-1">
+                  <FaUser />
+                  <input
+                    type="text"
+                    className="input-field flex-1"
+                    placeholder="Username"
+                    name="username"
+                    autoComplete="off"
+                    onChange={handleInputChange}
+                    value={formData.username}
+                  />
+                </label>
+                <label className="input input-bordered rounded flex items-center gap-2 flex-1">
+                  <MdDriveFileRenameOutline />
+                  <input
+                    type="text"
+                    className="input-field flex-1"
+                    placeholder="Full Name"
+                    name="fullName"
+                    autoComplete="off"
+                    onChange={handleInputChange}
+                    value={formData.fullName}
+                  />
+                </label>
+              </div>
+              <label className="input input-bordered rounded flex items-center gap-2">
+                <MdPassword />
+                <input
+                  type="password"
+                  className="input-field flex-1"
+                  placeholder="Password"
+                  name="password"
+                  autoComplete="off"
+                  onChange={handleInputChange}
+                  value={formData.password}
+                />
+              </label>
+              <button className="btn bg-green-900 text-white rounded-full">
+                {isPending ? "Loading..." : "Sign Up"}
+              </button>
+              {isError && <p className="text-red-500">{error.message}</p>}
+            </form>
+            <div className="flex flex-row gap-2 mt-4">
+              <p className="text-black text-lg">Already have an account?</p>
+              <Link to="/login" className="text-green-900 hover:underline">
+                Sign In
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -191,4 +136,3 @@ const SignUpPage = () => {
 };
 
 export default SignUpPage;
-
